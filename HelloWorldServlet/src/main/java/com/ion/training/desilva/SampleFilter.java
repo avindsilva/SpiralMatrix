d9829bd596da452806ade95fc64bce05d5d63242
Avin D'Silva <avindsilva@gmail.com>
2015-05-09 17:18:39 +0530

    Using filters

---
+++ HelloWorldServlet/src/main/java/com/ion/training/desilva/SampleFilter.java
@@ -0,0 +1,47 @@
+package com.ion.training.desilva;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class RequestLoggingFilter
+ */
+@WebFilter("/SampleFilter")
+public class SampleFilter implements Filter {
+
+    private ServletContext context;
+
+    public void init(FilterConfig fConfig) throws ServletException {
+        this.context = fConfig.getServletContext();
+        this.context.log("Sample Filter initialized");
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        Enumeration<String> params = req.getParameterNames();
+        while(params.hasMoreElements()){
+            String name = params.nextElement();
+            String value = request.getParameter(name);
+            this.context.log(req.getRemoteAddr() + "::Request Params::{"+name+"="+value+"}");
+        }
+
+        // pass the request along the filter chain
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        //we can close resources here
+    }
+
+}
